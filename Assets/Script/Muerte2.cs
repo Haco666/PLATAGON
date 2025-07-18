65c0a91e41dffdace344c85de48aec4548fa84b9
FelucoGC <diablo3_666@hotmail.com>
2025-07-18 12:53:27 +0100

    final1

--- Assets/Script/Muerte2.cs
+++ Assets/Script/Muerte2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
-public class Next2 : MonoBehaviour
+public class Muerte2 : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D collider){
     Debug.Log("se tocan ");
--- Assets/Script/Next3.cs
+++ Assets/Script/Next3.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
-public class Next2 : MonoBehaviour
+public class Next3 : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D collider){
     Debug.Log("se tocan ");
     if(collider.CompareTag("Player"))
        {
-    SceneManager.LoadScene("Level3", LoadSceneMode.Single);
+    SceneManager.LoadScene("SceneM", LoadSceneMode.Single);
         }// escena nueva 
     }
 }
